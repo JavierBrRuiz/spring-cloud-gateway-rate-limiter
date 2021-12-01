a6db5d2567ad4cb0213022e03c072485ab72683e
ctlove0523 <478309639@qq.com>
2021-12-01 15:54:07 +0800

    Update GatewayModuleApplication.java

--- src/main/java/com/deimos/sdemails/gatewaymodule/GatewayModuleApplication.java
+++ src/main/java/com/deimos/sdemails/gatewaymodule/GatewayModuleApplication.java
@@ -7,6 +7,7 @@ import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
+@ComponentScan("com.deimos.sdemails")
 public class GatewayModuleApplication {
 
 	public static void main(String[] args) {
